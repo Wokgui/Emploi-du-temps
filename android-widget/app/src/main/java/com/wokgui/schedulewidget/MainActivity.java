8cc94f80b04c30efd23281d9e937b3fce17e4edf
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-09-02 20:23:01 +0200

    Ajoute l’écran complet de l’emploi du temps

---
+++ android-widget/app/src/main/java/com/wokgui/schedulewidget/MainActivity.java
@@ -0,0 +1,19 @@
+package com.wokgui.schedulewidget;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        WebView webView = findViewById(R.id.webView);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        webView.loadUrl("file:///android_asset/index.html");
+    }
+}
