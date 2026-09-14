5644b4c4d6f5fe41c2f542e2af2f01c05513c374
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-09-14 08:40:30 +0200

    6.55: add mini planning service shell

---
+++ android-widget/app/src/main/java/com/wokgui/schedulewidget/MiniPlanningService.java
@@ -0,0 +1,11 @@
+package com.wokgui.schedulewidget;
+
+import android.content.Intent;
+import android.widget.RemoteViewsService;
+
+public final class MiniPlanningService extends RemoteViewsService {
+    @Override
+    public RemoteViewsFactory onGetViewFactory(Intent intent) {
+        return null;
+    }
+}
