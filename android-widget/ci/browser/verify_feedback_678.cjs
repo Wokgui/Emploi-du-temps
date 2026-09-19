1355bb7168c9be7bbe6b80039febc26322a6930a
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-09-19 20:30:49 +0200

    test: carry 6.78 regressions on 6.84

--- android-widget/ci/browser/verify_feedback_678.cjs
+++ android-widget/ci/browser/verify_feedback_678.cjs
@@ -139,8 +139,8 @@ const settle = page => page.evaluate(() => new Promise(resolve => requestAnimati
   assert.match(styles31, /windowSplashScreenAnimatedIcon">@drawable\/ic_splash_mark_safe/);
   assert.match(splash, /android:insetLeft="24dp"[\s\S]*android:insetBottom="24dp"/);
   assert.match(chunkSource, /Feedback678Ui\.script\(\)/);
-  assert.match(gradle, /versionCode 679001/);
-  assert.match(gradle, /versionName '6\.79'/);
+  assert.match(gradle, /versionCode 684001/);
+  assert.match(gradle, /versionName '6\.84'/);
   assert.deepEqual(errors, []);
   console.log('feedback_678_week_type_is_settings_only=passed');
   console.log('feedback_678_day_and_week_labels_follow_cycle=passed');
