b023d1654eeac6037538ba21fe63f39e10bd82c8
Wokgui <149906755+Wokgui@users.noreply.github.com>
2026-09-22 15:19:23 +0200

    7.45: load final settings owner last

--- android-widget/app/src/main/java/com/wokgui/schedulewidget/ChunkedUiScripts.java
+++ android-widget/app/src/main/java/com/wokgui/schedulewidget/ChunkedUiScripts.java
@@ -57,6 +57,7 @@ final class ChunkedUiScripts {
         add(out, Feedback676Ui.script());
         add(out, Feedback678Ui.script());
         add(out, WeekView744Ui.script());
+        add(out, Settings745Ui.script());
         if (BuildConfig.DEBUG) add(out, HeavyPanelPerformanceUi648.script());
         add(out, UiRuntimeBundle.idleImportScript());
         return out.toArray(new String[0]);
